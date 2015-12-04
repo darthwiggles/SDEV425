9d81a8afc008e9d9d3bf8090bda0258af6a2b5c0
darthwiggles <jeffdkeys@gmail.com>
2015-12-04 10:35:17 -0500

    Update SDEV425_2.java

--- SDEV425_2.java
+++ SDEV425_2.java
@@ -325,17 +325,21 @@ public class SDEV425_2 extends Application {
         int codeInt = ThreadLocalRandom.current().nextInt(10000000, 99999999);
         String code = Integer.toString(codeInt);
         
-        /*
-        Email does not function. "Could not convert socket to TLS."
+        
+        //Email does not function. "Could not convert socket to TLS."
         
         Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
-
+		//props.put("mail.smtp.host", "smtp.gmail.com");
+		//props.put("mail.smtp.socketFactory.port", "465");
+		//props.put("mail.smtp.socketFactory.class",
+		//		"javax.net.ssl.SSLSocketFactory");
+		//props.put("mail.smtp.auth", "true");
+		//props.put("mail.smtp.port", "465");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.port", "587");
+                props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
 		Session session = Session.getDefaultInstance(props,
 			new javax.mail.Authenticator() {
@@ -358,9 +362,9 @@ public class SDEV425_2 extends Application {
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
 		}
-            */
+            
         
-        System.out.println("Code is " + code + "\n");
+        //System.out.println("Code is " + code + "\n");
         return codeInt;
     }
 }
